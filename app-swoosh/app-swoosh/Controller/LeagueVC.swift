054664caa360e7159ef495a5f3f6dd22534f91f3
Gustav <gustav@Gustavs-MacBook-Air.local>
2018-04-28 10:42:25 +0200

    some changes have been made

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
     @IBAction func onNextTapped(_ sender: Any) {
-        performSegue(withIdentifier: "skillVCSegue", sender: <#T##Any?#>)
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       player = Player()
     }
 
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        player.desiredLeague = "mens"
+        nextBtn.isEnabled = true
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        player.desiredLeague = "womens"
+        nextBtn.isEnabled = true
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        player.desiredLeague = "coed"
+        nextBtn.isEnabled = true
+    }
+    
+    
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Gustav on 27/04/2018.
+//  Copyright © 2018 Frohlich Studio. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
